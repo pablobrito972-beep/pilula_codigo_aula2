8ae13ae3e6f633a514a6e52255cbb9cca2a8f50b
pablobrito972-beep <pablobrito972@gmail.com>
2026-03-13 19:50:44 -0300

    Refine output messages in pilula1.py
    Corrected output formatting for estimated subscribers and revenue.

--- pilula1.py
+++ pilula1.py
@@ -7,8 +7,6 @@ meses= int(input('digite a qtd de meses da projeção:'))
 #processamento
 assinantes_finais = assinantes * math.pow((1+taxa),meses)
 receita_final=assinantes_finais * mensalidades
-print(f'assinantes estimados:.{assinantes_finais:.0f}')
-receita_final= assinantes_finais * mensalidades
 #saida
-print(f'assinantes estimados{assinantes_finais:.0f}')
-print(f'receita estimada:R${receita_final:.2f}')
+print(f'assinantes estimados: {assinantes_finais:.0f}')
+print(f'receita estimada: R${receita_final:.2f}')
